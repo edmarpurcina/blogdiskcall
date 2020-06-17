40dc6068ad8ebd70f40e9bfb011797d22e6caebb
Edmar Purcina <edmar.purcina@gmail.com>
2020-06-17 11:58:07 -0300

    criação de templates

--- loginblog/views.py
+++ loginblog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 def ola_mundo(request):
-    a = ["<h1> Ola mundo </h1>"]
-    return render(request, a)
+    a = {"<h1> Ola mundo </h1>"}
+    return render( request, "template.html")
