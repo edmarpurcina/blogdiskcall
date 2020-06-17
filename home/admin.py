0bd965f4e6960f6f489afadaf30bb739c15f83e7
Edmar Purcina <edmar.purcina@gmail.com>
2020-06-17 16:15:18 -0300

    Criação banco de dados dos artigos

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Artigo
 
 # Register your models here.
+
+admin.site.register(Artigo)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Artigo(models.Model):
+
+    artigo_id = models.AutoField(primary_key=True)
+    artigo_autor_nome = models.CharField(max_length=40)
+    artigo_data_criacao = models.DateField(auto_now_add=True)
+    aritcle_data_modificacao = models.DateField(auto_now_add=True)
+    artigo_titulo = models.CharField(max_length=20)
+    artigo_texto = models.CharField(max_length=1024)
+    artigo_autor_ip = models.CharField(max_length=15)
+
+    def __ster__(self):
+        return self.artigo_title
